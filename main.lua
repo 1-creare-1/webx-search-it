941075757e29bea9f46cf9e638eef6ecee32b987
_creare_ <33350279+1-creare-1@users.noreply.github.com>
2024-05-31 16:49:54 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 if get == nil then get, fetch = require(game:GetService("ServerStorage").Types) end
 
-get("version").set_content("v0.0.6")
+get("version").set_content("v0.0.7")
 local query = get("query")
 local cards = get("card", true);
 
@@ -20,6 +20,48 @@ end
 
 function warn(msg)
 	errorLabel.set_content(msg)
+end
+
+-- Returns the Levenshtein distance between the two given strings
+function levenshtein(str1, str2)
+	local len1 = string.len(str1)
+	local len2 = string.len(str2)
+	local matrix = {}
+	local cost = 0
+
+	-- quick cut-offs to save time
+	if (len1 == 0) then
+		return len2
+	elseif (len2 == 0) then
+		return len1
+	elseif (str1 == str2) then
+		return 0
+	end
+
+	-- initialise the base matrix values
+	for i = 0, len1, 1 do
+		matrix[i] = {}
+		matrix[i][0] = i
+	end
+	for j = 0, len2, 1 do
+		matrix[0][j] = j
+	end
+
+	-- actual Levenshtein algorithm
+	for i = 1, len1, 1 do
+		for j = 1, len2, 1 do
+			if (str1:byte(i) == str2:byte(j)) then
+				cost = 0
+			else
+				cost = 1
+			end
+
+			matrix[i][j] = math.min(matrix[i-1][j] + 1, matrix[i][j-1] + 1, matrix[i-1][j-1] + cost)
+		end
+	end
+
+	-- return the last value - this is the Levenshtein distance
+	return matrix[len1][len2]
 end
 
 function query_domain(ip)
@@ -49,7 +91,12 @@ function load_domains()
 end
 
 function sort_domains(domains, query)
-	return domains
+	local sorted = domains
+	table.sort(sorted, function(a, b)
+		return levenshtein(query, a) < levenshtein(query, b)
+	end)
+	
+	return sorted
 end
 
 function percentage(value, min, max)
