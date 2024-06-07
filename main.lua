ec5272f52ac8171f0b0f59a7f8d69db00b7b8548
creare <makerbenjammin@gmail.com>
2024-06-06 20:29:19 -0400

    Add feeling lucky button functionality and multi-word queries

--- main.lua
+++ main.lua
@@ -1,43 +1,33 @@
 local get, fetch = get, fetch
 
-local VERSION = "v1.1.0"
-local API_URL = "https://webx-external-api.vercel.app/api/v1"
--- local API_URL = "http://127.0.0.1:5000/api/v1"
-
-get("version").set_content(VERSION)
-
 -- Debug Helpers
 local outputLabel, errorLabel = get("output"), get("error")
 function log(msg)
-	outputLabel.set_content(msg)
+	outputLabel.set_content(tostring(msg) .. "\n" .. outputLabel.get_content())
 end
 
 function warn(msg)
-	errorLabel.set_content(msg)
+	errorLabel.set_content(tostring(msg) .. "\n" .. errorLabel.get_content())
 end
 
+-- Constants
+local VERSION = "v1.1.1"
+local API_URL = "https://webx-external-api.vercel.app/api/v1"
+-- local API_URL = "http://127.0.0.1:5000/api/v1"
+
+-- Init
+get("version").set_content(VERSION)
 log("Loaded")
+
+-- Error Handling
 xpcall(function()
 
+-- Search box
 local queryTextBox = get("query")
 
--- Get the cards
+-- Get the search result cards
 local cards = {}
 do
---[[
-
-<div>
-	<h6 class="card_icon">ICON</h6>
-	<div class="card_sublist">
-		<p class="card_sitename">Website Name</p>
-		<p class="card_url">buss://websitename.tld</p>
-	</div>
-</div>
-
-<a href="https://example.com" class="card_title">Website Title - Cool Stuff</a>
-<p class="card_description">Website Description lore ipsum lore ipsum lore ipsum lore ipsum lore ipsum lore ipsum lore ipsum </p>
-<p class="card_score">Rank 0</p>
-]]
 	local cardHolders = get("card", true)
 
 	local icons = get("card_icon", true)
@@ -63,45 +53,20 @@ end
 log("Got cards")
 local page = 0
 
-local function make_query(term, limit, fuzziness)
+-- Generic request function to my API
+local function request(endpoint)
 	-- Do not be the reason I need to add a ratelimit to this
 	local res = fetch({
-		url = API_URL .. "/search?q="..term.."&fuzziness="..fuzziness.."&limit="..limit,
+		url = API_URL .. endpoint,
 		method = "GET",
 		headers = { ["Content-Type"] = "application/json" },
 		body = '',
 	})
 
-	if res.error then
-		return false, res.error
-	end
-	-- log("Got " .. #res .. " results: " .. res)
-	return true, res
+	return res.success, res
 end
 
-local function query_site_count()
-	-- Do not be the reason I need to add a ratelimit to this
-	local res = fetch({
-		url = API_URL .. "/count",
-		method = "GET",
-		headers = { ["Content-Type"] = "application/json" },
-		body = '',
-	})
-
-	if res.success then
-		return true, res.count
-	else
-		return false, res.error
-	end
-end
-local TOTAL_DOMAINS = "ERROR"
-do
-	local success, count = query_site_count()
-	if success then
-		TOTAL_DOMAINS = count
-	end
-end
-
+-- Set the search result cards to show data from a table of results
 local function render_cards(results)
 	for i, card in pairs(cards) do
 		local result = results[i]
@@ -132,6 +97,16 @@ local function render_cards(results)
 	end
 end
 
+-- URL Encode: https://smolkit.com/blog/posts/how-to-url-encode-in-lua/
+local function url_encode(str)
+	str = string.gsub(str, "([^%w%.%- ])", function(c)
+	  return string.format("%%%02X", string.byte(c))
+	end)
+	str = string.gsub(str, " ", "+")
+	return str
+end
+
+-- Split string on seperator character
 local function split_string(inputstr, sep)
 	if sep == nil then
 	  sep = "%s"
@@ -141,7 +116,7 @@ local function split_string(inputstr, sep)
 	  table.insert(t, str)
 	end
 	return t
-  end
+end
 
 -- function SetPage(newPage)
 -- 	page = newPage
@@ -158,11 +133,11 @@ local function split_string(inputstr, sep)
 -- 	log("Page set to " .. page)
 -- end
 
-function Search(content)
+local function Search(content)
 	xpcall(function()
-		local firstWord = split_string(content, " ")[1]
-		log("Searching for " .. firstWord)
-		local success, result = make_query(firstWord, #cards, 0.4)
+		-- local firstWord = split_string(content, " ")[1]
+		log("Searching for " .. content)
+		local success, result = request("/search?q="..url_encode(content).."&fuzziness="..(0.4).."&limit="..#cards)
 		if success then
 			render_cards(result.domains)
 			-- Show elapsed time & results
@@ -170,15 +145,36 @@ function Search(content)
 			get("query_results").set_content("About "..#result.domains.." results ("..elapsed.." seconds)")
 		else
 			render_cards({})
-			log(result)
+			log(result.error)
 		end
 	end,warn)
 end
 
-get("info_header").set_content("About " .. TOTAL_DOMAINS .. " total Websites! Please give up to 24 hours for your domain to be listed. Created by _creare_")
+local TOTAL_DOMAINS = "ERROR"
+do
+	local success, response = request("/count")
+	if success then
+		TOTAL_DOMAINS = response.count
+	else
+		warn(response.error)
+	end
+end
+
+get("info_header").set_content("About " .. TOTAL_DOMAINS .. " Websites! Please give up to 24 hours for your domain to be listed. Created by _creare_")
 get("searchbtn").on_click(function()
 	Search(queryTextBox.get_content())
 end)
+get("luckybtn").on_click(function()xpcall(function()
+	local success, response = request("/random")
+	if success then
+		render_cards({response.page})
+		-- Show elapsed time & results
+		local elapsed = math.floor((response['elapsed_time'] + 0.5) * 100) / 100
+		get("query_results").set_content("Random site in " .. elapsed .. " seconds")
+	else
+		warn(response.error)
+	end
+end, warn)end)
 
 queryTextBox.on_submit(Search)
 
